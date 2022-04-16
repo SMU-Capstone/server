11584635c1ee24d02ff99e2fb680af2284ca78b7
lunar <mo0nlight0@naver.com>
2022-04-16 16:58:07 +0900

    Setting: Swagger base setting. #0

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  /* Swagger setting */
+  const config = new DocumentBuilder()
+    .setTitle('쓰레기통 알리미')
+    .setDescription('쓰레기통 알리미 API 명세서')
+    .setVersion('1.0')
+    .addTag('Trashcan')
+    .addTag('Application')
+    .addTag('Cleaning')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
