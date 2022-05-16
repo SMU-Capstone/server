f04e2a80d4431bd64a99766593931e9e71610242
hyeju <hyeju724@naver.com>
2022-05-16 17:21:48 +0900

    Update application.service.ts

--- src/domain/application/application.service.ts
+++ src/domain/application/application.service.ts
@@ -1,26 +1,49 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Application } from 'entities/Application';
+import { Repository } from 'typeorm';
 import { CreateApplicationDto } from '../../dto/application/create-application.dto';
 import { UpdateApplicationDto } from '../../dto/application/update-application.dto';
 
 @Injectable()
 export class ApplicationService {
-  create(createApplicationDto: CreateApplicationDto) {
-    return 'This action adds a new application';
+  constructor(
+    @InjectRepository(Application)
+    private readonly applicationRepository: Repository<Application>,
+  ) {}
+
+  /*새 쓰레기통 추가 신청을 DB에 저장: create*/
+  async create(createApplicationDto: CreateApplicationDto): Promise<void> {
+    await this.applicationRepository.save({
+      note: createApplicationDto.note,
+      type: createApplicationDto.type,
+      latitude: createApplicationDto.latitude,
+      longitude: createApplicationDto.longitude,
+    });
   }
 
-  findAll() {
-    return `This action returns all application`;
+  /* DB 내 모든 쓰레기통 추가 신청을 조회한다. */
+  async findAll() : Promise<Application[] | null> {
+    return this.applicationRepository.find();
+  }
+  
+  /* DB에서 특정 ID의 쓰레기통 추가신청을 조회한다. */
+  async findOne(id: number) : Promise<Application | null> {
+    return this.applicationRepository.findOneBy({id : id});
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} application`;
+  //update
+  async update(id: number, updateApplicationDto: UpdateApplicationDto) : Promise<void> {
+    await this.applicationRepository.update(
+      id,
+      updateApplicationDto
+    );
   }
 
-  update(id: number, updateApplicationDto: UpdateApplicationDto) {
-    return `This action updates a #${id} application`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} application`;
+  // SOFT DELETE
+  async remove(id: number) : Promise<void> { 
+    await this.applicationRepository.softDelete(
+      id
+    );
   }
 }
