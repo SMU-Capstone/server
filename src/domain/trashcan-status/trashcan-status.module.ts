ccf7e698273df8f4d90aeb0b96e3ba047497841c
Jong-hyun <mo0nlight0@naver.com>
2022-08-19 01:10:34 +0900

    Fix: Import TypeORM module at trashcan-status module.

--- src/domain/trashcan-status/trashcan-status.module.ts
+++ src/domain/trashcan-status/trashcan-status.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TrashcanStatusService } from './trashcan-status.service';
 import { TrashcanStatusController } from './trashcan-status.controller';
+import { TrashcanStatus } from 'entities/TrashcanStatus';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TrashcanStatus])], 
   providers: [TrashcanStatusService],
   controllers: [TrashcanStatusController]
 })
