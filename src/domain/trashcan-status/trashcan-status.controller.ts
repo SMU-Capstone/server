ffe86ba43df97ff5a11a73dc4af59aa07a436268
Jong-hyun <mo0nlight0@naver.com>
2022-08-19 14:30:17 +0900

    Docs: Add documents and annotations at trashcan-status files.

--- src/domain/trashcan-status/trashcan-status.controller.ts
+++ src/domain/trashcan-status/trashcan-status.controller.ts
@@ -1,26 +1,57 @@
 import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { ApiBody, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
 import { CreateTrashcanStatusDto } from 'dto/trashcan-status/create-trashcan-status.dto';
 import { TrashcanStatusService } from './trashcan-status.service';
 
+@ApiTags('Trashcan-Status')
 @Controller('trashcan-status')
 export class TrashcanStatusController {
     constructor(private readonly trashcanStatusService: TrashcanStatusService) {}
 
+    @ApiOperation({
+        summary: '쓰레기통 상태 정보를 DB에 저장한다.',
+    })
+    @ApiBody({
+        type: CreateTrashcanStatusDto,
+        description: '쓰레기통 상태 정보'
+    })
     @Post()
     create(@Body() createTrashcanStatusDto: CreateTrashcanStatusDto) {
         return this.trashcanStatusService.create(createTrashcanStatusDto);    
     }
 
+    @ApiOperation({
+        summary: '특정 ID를 가진 쓰레기통 상태 정보를 반환한다.',
+    })
+    @ApiParam({
+        name: 'id',
+        description: '쓰레기통 상태 정보의 ID',
+        example: '1',
+        required: true,
+    })
     @Get(':id')
     findOne(@Param('id') id: number) {
         return this.trashcanStatusService.findOne(id);
     }
 
+    @ApiOperation({
+        summary: 'DB에 저장된 모든 쓰레기통 상태 정보를 반환한다.',
+    })
     @Get()
     findAll() {
         return this.trashcanStatusService.findAll();
     }
+
     
+    @ApiOperation({
+        summary: '특정 ID를 가진 쓰레기통 상태 정보를 삭제한다.',
+    })
+    @ApiParam({
+        name: 'id',
+        description: '삭제하고자 하는 청소 신청의 ID',
+        example: '1',
+        required: true
+    })
     @Delete(':id')
     remove(@Param('id') id: number) {
         return this.trashcanStatusService.remove(id);
--- src/domain/trashcan-status/trashcan-status.service.ts
+++ src/domain/trashcan-status/trashcan-status.service.ts
@@ -11,6 +11,7 @@ export class TrashcanStatusService {
         private readonly trashcanStatusRepository: Repository<TrashcanStatus>,
     ) {}
 
+    /* 새 쓰레기통 상태 정보를 DB 내에 생성한다. */
     async create(createTrashcanStatusDto: CreateTrashcanStatusDto): Promise<void> {
         await this.trashcanStatusRepository.save({
             isFull: createTrashcanStatusDto.isFull,
@@ -18,15 +19,17 @@ export class TrashcanStatusService {
         });
     }
 
-
+    /* DB에서 특정 ID를 가진 상태 정보를 조회한다. */
     findOne(id: number): Promise<TrashcanStatus> {
         return this.trashcanStatusRepository.findOne(id);
     }
 
+    /* DB 내의 모든 상태 정보를 조회한다. */
     findAll(): Promise<TrashcanStatus[]> {
         return this.trashcanStatusRepository.find();
     }
 
+    // SOFT DELETE
     async remove(id: number): Promise<void> {
         await this.trashcanStatusRepository.softDelete(
             id
